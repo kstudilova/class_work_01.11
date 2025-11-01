db53f34482f322e0f12d8e95f24a724b4c092db2
kstudilova <kstudilova@gmail.com>
2025-11-01 13:06:50 +0300

    Create and destroy

--- main.cpp
+++ main.cpp
@@ -1,4 +1,44 @@
 #include <iostream>
-int main() {
-    std::cout << "Hello, world!\n";
+
+void destroy(int ** mtx, size_t created) 
+{
+    for (size_t i = 0; i < created; i++)
+    {
+        delete[] mtx[i];
+    }
+    delete[] mtx;
+}
+
+int ** create(size_t rows, size_t cols)
+{
+    int ** mtx  = new int * [rows];
+    size_t created = 0;
+    try 
+    {
+        for (; created < rows; ++created) //(size_t i = 0; i < rows; ++i)
+        {
+            mtx[created]  = new int[cols]; //new может сгенерировать исключение 
+        }
+    }
+    catch (const std::bad_alloc & e)
+    {
+        destroy(mtx, created);
+        throw; //опрокинится в вызывающую функцию main
+    }
+    return mtx;
+}
+
+int main()
+{
+    int ** matrix = nullptr;
+    try
+    {
+        matrix = create(5, 5);
+    }
+    catch(const std::bad_alloc & e)
+    {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
+    destroy(matrix, 5);
 }
