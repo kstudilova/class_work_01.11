2582b3eb24005787c2b734a564343b6483e1dab0
kstudilova <kstudilova@gmail.com>
2025-11-01 22:00:23 +0300

    Splitting a one-dimensional array

--- main.cpp
+++ main.cpp
@@ -50,18 +50,48 @@ void input(int ** mtx, size_t rows, size_t cols)
     }
 }
 
-void output(int ** mtx, size_t rows, size_t cols)
+void output(int ** mtx, const size_t * lns, size_t rows)
 {
     for (size_t i = 0; i < rows; ++i)
     {
         std::cout << ' ' << mtx[i][0];
-        for (size_t j = 1; j < cols; ++j)
+        for (size_t j = 1; j < lns[i]; ++j)
         {
             std::cout << " " << mtx[i][j];
         }
         std::cout <<  "\n";
     }
 
+}
+
+int ** convert(const int * t, size_t n, const size_t * lns, size_t rows)
+{
+    int ** matrix = new int * [rows];
+    size_t created = 0;
+    size_t next_lns = 0;
+    try
+    {
+        for (size_t i = 0; i < rows; ++i)
+        {
+            matrix[i] = new int[lns[i]];
+            for (size_t j = 0; j < lns[i]; ++j)
+            {
+                matrix[i][j] = t[next_lns + j];
+            }
+            next_lns += lns[i];
+            ++created;
+        }
+    } 
+    catch (...) 
+    {
+        for (size_t i = 0; i < created; ++i)
+        {
+            delete[] matrix[i];
+        }
+        delete[] matrix;
+        throw;
+    }
+    return matrix;
 }
 
 int main()
@@ -73,11 +103,11 @@ int main()
         std::cerr << "Incorrect input\n";
         return 2;
     }
-
+    
     int ** matrix = nullptr;
     try
     {
-        matrix = create(5, 5);
+        matrix = create(r, c);
     }
     catch(const std::bad_alloc & e)
     {
@@ -97,4 +127,22 @@ int main()
     output(matrix, r, c);
 
     destroy(matrix, r);
+
+    int one_dim_array[] = {5,5,5,5,6,6,7,7,7,7,8};
+    size_t n = 11;
+    size_t lns[] = {4,2,4,1};
+    size_t rows = 4;
+    try {
+        int** mat = convert(one_dim_array, n, lns, rows);
+        output(mat, lns, rows);
+        destroy(mat, rows);     
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << '\n';
+        delete[] one_dim_array;
+        delete[] lns;
+        return 3;
+    }
+
+    delete[] one_dim_array;
+    delete[] lns;
 }
